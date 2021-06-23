ffd657460b0c9b7ac6df657e1ad94cd40e672eb3
Adi Carlisle <adicarlisle@gmail.com>
2021-06-23 14:23:11 +0000

    URI issues

--- application/routes.py
+++ application/routes.py
@@ -4,6 +4,12 @@ from flask import render_template
 
 db.drop_all()
 db.create_all()
+adi = Users(username="Adi", email="adicarlisle@gmail.com", password="password", first_name="Adi",last_name="Carlisle")
+dice1 = Dice(level=2, range=10)
+entry = History(user_id=1,dice_id=1)
+db.session.add(adi)
+db.session.add(dice1)
+db.session.add(entry)
 db.session.commit()
 @app.route("/")
 def home():
