b6265247b88a0caeb89bb2356740057198d70278
Adi Carlisle <adicarlisle@gmail.com>
2021-06-23 15:15:40 +0000

    Removedall db

--- application/routes.py
+++ application/routes.py
@@ -5,14 +5,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 #Testing the database with dummy data
-db.drop_all()
-db.create_all()
-adi = Users(username="Adi", email="adicarlisle@gmail.com", password="password", first_name="Adi",last_name="Carlisle")
-dice1 = Dice(level=2, range=10)
-db.session.add(adi)
-db.session.commit()
-db.session.add(dice1)
-db.session.commit()
+# db.drop_all()
+# db.create_all()
+# adi = Users(username="Adi", email="adicarlisle@gmail.com", password="password", first_name="Adi",last_name="Carlisle")
+# dice1 = Dice(level=2, range=10)
+# db.session.add(adi)
+# db.session.commit()
+# db.session.add(dice1)
+# db.session.commit()
 # entry = History(user = Users.query.filter_by(id=1).first(), dice = Dice.query.filter_by(id=1).first())
 # db.session.add(entry)
 # db.session.commit()
