261c2c6f00b3153702ad77c3e88e6920d92dea84
Adi Carlisle <adicarlisle@gmail.com>
2021-06-23 15:42:11 +0000

    DROP TABLE was the problem

--- application/routes.py
+++ application/routes.py
@@ -5,7 +5,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 #Testing the database with dummy data
-db.drop_all()
 db.create_all()
 adi = Users(username="Adi", email="adicarlisle@gmail.com", password="fakepassword", first_name="Adi",last_name="Carlisle")
 dice1 = Dice(level=2, range=10)
